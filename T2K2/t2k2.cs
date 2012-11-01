1d02643465b7fa8dfaebfd70c515d2de38689a8c
voxel01 <lukas01@gmx.at>
2012-11-01 19:30:56 +0100

    Anwendung der Diff methode

--- T2K2/t2k2.cs
+++ T2K2/t2k2.cs
@@ -9,6 +9,13 @@ namespace T2K2
     {
         static void Main(string[] args)
         {
+            Uhrzeit t1 = new Uhrzeit(3, 2, 1);
+            Uhrzeit t2 = new Uhrzeit(5, 4, 3);
+
+            int hh, mm, ss;
+            t1.Diff(t2, out hh, out mm, out ss);
+
+            Console.WriteLine("Differenz zwischen t1 und t1 sind {0} Stunden, {1} Minuten und {2} Sekunden", hh, mm, ss);
 
         }
     }
